c2baf88fa966e1fc49a76de41094d39462180e78
mykytagrusha <mykyta.grusha@collibra.com>
2020-09-20 22:50:44 +0200

    Updates

---
+++ precache-manifest.a9f4861d21043107a526384db32df7e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d992ac37f0550d15276afb099338f3eb",
+    "url": "/sna/index.html"
+  },
+  {
+    "revision": "684b212c150e1e05b2ac",
+    "url": "/sna/static/css/main.2fa38bda.chunk.css"
+  },
+  {
+    "revision": "641adac071b7068437d2",
+    "url": "/sna/static/js/2.f17ed9c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sna/static/js/2.f17ed9c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684b212c150e1e05b2ac",
+    "url": "/sna/static/js/main.f93b3816.chunk.js"
+  },
+  {
+    "revision": "3a567dfe48a66ae1b955",
+    "url": "/sna/static/js/runtime-main.1fc66799.js"
+  }
+]);
